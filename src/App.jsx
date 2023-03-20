39398428ae137c37473a51edcf7f4c25f2b5a0ab
Daniel Ramirez <ramirezdani016@hotmail.com>
2023-03-20 17:37:47 -0300

    se agrego chakra-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { Spinner, Stack } from "@chakra-ui/react";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hola mundo</h1>
-    </div>
+    <>
+      <Stack direction="row" spacing={4}>
+        <Spinner size="xs" />
+        <Spinner size="sm" />
+        <Spinner size="md" />
+        <Spinner size="lg" />
+        <Spinner size="xl" />
+      </Stack>
+    </>
   );
 }
 
