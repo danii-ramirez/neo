9f7f664ca75e82e7a0e1bd06878253921068ee9f
Daniel Ramirez <ramirezdani016@hotmail.com>
2023-04-13 18:30:21 -0300

    prueba de loader

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Wrap, WrapItem } from "@chakra-ui/react";
 import ItemList from "./ItemList";
+import Loading from "./Loader";
 
 const ItemListContainer = () => {
   const getProducts = async () => {
@@ -17,36 +18,48 @@ const ItemListContainer = () => {
   };
 
   const [products, setProducts] = useState([]);
-  const { category } = useParams();
   const [loading, setLoading] = useState(true);
+  const { category } = useParams();
 
   useEffect(() => {
-    if (category === undefined) {
-      getProducts().then((data) => setProducts(data));
-    } else {
-      getProductsByCategory().then((data) => setProducts(data));
-    }
+    setTimeout(() => {
+      if (category === undefined) {
+        getProducts().then((data) => setProducts(data));
+      } else {
+        getProductsByCategory().then((data) => setProducts(data));
+      }
+
+      setLoading(false);
+    }, 5000);
   }, [category]);
 
-  return (
-    <>
-      <Wrap>
-        {products.map((prod) => {
-          return (
-            <WrapItem key={prod.id}>
-              <ItemList
-                id={prod.id}
-                title={prod.title}
-                image={prod.image}
-                category={prod.category}
-                count={prod.rating.count}
-              />
-            </WrapItem>
-          );
-        })}
-      </Wrap>
-    </>
-  );
+  if (loading) {
+    return (
+      <>
+        <Loading />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Wrap>
+          {products.map((prod) => {
+            return (
+              <WrapItem key={prod.id}>
+                <ItemList
+                  id={prod.id}
+                  title={prod.title}
+                  image={prod.image}
+                  category={prod.category}
+                  count={prod.rating.count}
+                />
+              </WrapItem>
+            );
+          })}
+        </Wrap>
+      </>
+    );
+  }
 };
 
 export default ItemListContainer;
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,15 @@
-import Spinner from "react-bootstrap/Spinner";
+import { Spinner } from "@chakra-ui/react";
 
 function Loading() {
-  return <Spinner animation="border" />;
+  return (
+    <>
+      <div className="row justify-content-center mt-5">
+        <div className="col-auto">
+          <Spinner size="xl" />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Loading;
